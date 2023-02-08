01e1652e75869ee279d96163ae400f386cf2d423
souvikadhikary <souvik500>
2023-02-08 19:53:51 +0530

    Encapsulation file

--- src/main/java/com/driver/Main.java
+++ src/main/java/com/driver/Main.java
@@ -1,5 +1,9 @@
 package com.driver;
 
 public class Main {
-  
+    public static void main(String[] args) {
+        RWOnly rw = new RWOnly();
+        //rw.setName("Souvik");
+        System.out.println(rw.getName());
+    }
 }
